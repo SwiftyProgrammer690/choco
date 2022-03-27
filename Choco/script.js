d7ef53c3d53d589a19d075f6692149af33fe4ac0
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2022-03-27 16:34:10 -0400

    Add more features

--- Choco/script.js
+++ Choco/script.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-rl.question("What do you want to do? A. Generate a random game, B. Generate random game + get a visual, C. Random Dice roll and if u are super unlucky then u get a 0\n", function (answer) {
+rl.question("What do you want to do? A. Generate a random game, B. Generate random game + get a visual, C. Random Dice roll and if u are super unlucky then u get a 0, D. 8ball\n", function (answer) {
     if (answer == "A") {
         while (!chess.game_over()) {
             const moves = chess.moves()
@@ -29,7 +29,16 @@ rl.question("What do you want to do? A. Generate a random game, B. Generate rand
         console.log(chess.ascii())
     }
     else if (answer == "C") {
-	console.log(Math.Random() * 6)
+	console.log(Math.floor(Math.random() * 7))
+    }
+    else if (answer == "D") {
+	const responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+             "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+             "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+             "Yes.", "Yes – definitely.", "You may rely on it."]
+
+	console.log("Think about you question in your head")
+	console.log(responses[Math.floor(Math.random() * 20)])
     }
     else {
         console.log("Please enter the command correctly!");
